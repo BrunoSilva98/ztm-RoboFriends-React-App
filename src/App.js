e24741bf4e68827b98cd86d003fd21d82440d713
BrunoSilva98 <brunops98@hotmail.com>
2020-02-06 19:09:34 -0300

    Ajuste de fonte

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox'
 import {robots} from './robots';
-
+import './App.css'
 class App extends  React.Component {
     
     constructor() {
@@ -26,7 +26,7 @@ class App extends  React.Component {
         return (
             <Fragment>
                 <header className='tc'>
-                    <h1>RoboFriends</h1>
+                    <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
                 </header> 
                 <CardList robots={filteredRobots}/>  
