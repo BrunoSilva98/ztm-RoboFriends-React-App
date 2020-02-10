64737b164f0206fbf7f88c6be6b45e0ea86fdea3
BrunoSilva98 <brunops98@hotmail.com>
2020-02-10 19:16:49 -0300

    Separacao de arquivos por pasta

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,22 @@
 import React, { Fragment } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox'
-import {robots} from './robots';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox'
+import Scroll from "../components/Scroll";
 import './App.css'
 class App extends  React.Component {
     
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchField: ''
         }    
+    }
+
+    componentDidMount(){
+        fetch(`https://jsonplaceholder.typicode.com/users`)
+            .then(response => response.json())
+            .then(users => this.setState({robots: users}));
     }
 
     onSearchChange = (event) => {
@@ -29,7 +35,9 @@ class App extends  React.Component {
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
                 </header> 
-                <CardList robots={filteredRobots}/>  
+                <Scroll>
+                    <CardList robots={filteredRobots}/>  
+                </Scroll>
             </Fragment>
 
         );
