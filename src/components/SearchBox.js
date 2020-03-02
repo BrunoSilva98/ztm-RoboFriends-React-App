960cdee88bd38e2dae3709bf4516a8af7eda466c
brunosilva98 <brunops98@hotmail.com>
2020-03-01 22:05:11 -0300

    Correcao np app.js

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 const SearchBox = ({searchChange}) =>{
-    return(
-        <Fragment>
-            <div className='pa2'>
-                <input className='pa3 ba b--green bg-lightest-blue' 
-                        type='search' 
-                        placeholder='Search robots'
-                        onChange={searchChange}/>
-            </div>
-        </Fragment>
-    );
+    return (
+        <div className='pa2'>
+          <input
+            className='pa3 ba b--green bg-lightest-blue'
+            type='search'
+            placeholder='search robots'
+            onChange={searchChange}
+          />
+        </div>
+      );
 }
 
 export default SearchBox;
